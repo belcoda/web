791be45017a14536a3a48c91ebce7bf674c8b87e
Django Merope <dmerope@gmail.com>
2025-11-14 20:39:56 +0900

    Add sidebar provider to main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,18 @@
 	import favicon from '$lib/assets/favicon.png';
 
 	let { children } = $props();
+	import * as Sidebar from '$lib/comps/ui/sidebar/index.js';
+	import NavSidebar from '$lib/comps/nav/Sidebar.svelte';
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 	<title>Belcoda</title>
 </svelte:head>
-<main class="h-screen">
-	{@render children?.()}
-</main>
+
+<Sidebar.Provider>
+	<NavSidebar />
+	<main class="h-screen">
+		{@render children?.()}
+	</main>
+</Sidebar.Provider>
