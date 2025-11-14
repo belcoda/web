75a7abafa377b920ce4e479aa8836f6dfbd22e17
Django Merope <dmerope@gmail.com>
2025-11-14 18:44:48 +0900

    Blog page

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	const { data } = $props();
+	import BlogPageLayout from '$lib/comps/blog/BlogPageLayout.svelte';
+	import BlogPostCard from '$lib/comps/blog/BlogPostCard.svelte';
+</script>
+
+<BlogPageLayout>
+	{#each data.posts as item}
+		<BlogPostCard post={item.post} author={item.author} />
+	{/each}
+</BlogPageLayout>
