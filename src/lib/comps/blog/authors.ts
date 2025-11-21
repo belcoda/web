b2bb7579246157f98bd1a340ac646446c5473958
Django Merope <dmerope@gmail.com>
2025-11-21 22:14:23 +0900

    Update author image

--- src/lib/comps/blog/authors.ts
+++ src/lib/comps/blog/authors.ts
@@ -2,8 +2,7 @@ const authors = [
 	{
 		name: 'Django Merope',
 		slug: 'django-merope',
-		avatar:
-			'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+		avatar: '/avatars/django-img.jpg',
 		title: 'Project lead'
 	},
 	{
